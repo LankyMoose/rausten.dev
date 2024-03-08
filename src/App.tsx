78387a8f94aea976762122bee0e9dff2d2c96909
Robby6Strings <rausten93@gmail.com>
2024-03-09 08:14:04 +1300

    update github link

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,6 @@ export function App() {
       </header>
       <main>
         <HeroSection />
-        <PreviousWork />
       </main>
       <footer>
         <div className="flex justify-end">
@@ -50,7 +49,7 @@ function Nav() {
           Resume
         </a>
         <a
-          href="https://github.com/Robby6Strings"
+          href="https://github.com/LankyMoose"
           target="_blank"
           className="rounded-full border-2 border-current p-1"
         >
@@ -81,9 +80,6 @@ function HeroSection() {
             I'm a software developer focussed on building interactive
             applications & tools that make building web applications easier.
           </p>
-          {/* <a href="#previous-work" className="button-link text-xl">
-            Previous Work
-          </a> */}
         </div>
       </div>
       <div id="skills-list">
@@ -96,59 +92,5 @@ function HeroSection() {
         <CssIcon />
       </div>
     </section>
-  )
-}
-
-function PreviousWork() {
-  return null
-  return (
-    <section id="previous-work">
-      <h2 className="text-center text-3xl font-bold">
-        <span className="inline-block">
-          <span className="px-5 mb-2">Previous Work</span>
-          <hr className="border border-white opacity-75" />
-        </span>
-      </h2>
-      <PreviousWorkItem
-        data={{
-          title: "Karapoti Consulting",
-          desc: "Planning, prototyping & implementing product expansions",
-          imgSrc: "https://www.karapoti.com/uploads/logo.png",
-          tags: [
-            { text: "C#", color: "purple" },
-            { text: ".NET", color: "cornflowerblue" },
-            { text: "SQL", color: "red" },
-            { text: "JS", color: "gold" },
-          ],
-        }}
-      />
-    </section>
-  )
-}
-
-interface WorkItemData {
-  title: string
-  desc: string
-  imgSrc: string
-  tags: { text: string; color: string }[]
-}
-
-function PreviousWorkItem({ data }: { data: WorkItemData }) {
-  return (
-    <div className="item-card">
-      <img src={data.imgSrc} />
-      <h3>{data.title}</h3>
-      <p>{data.desc}</p>
-      <ul className="badge-list">
-        {data.tags.map((t) => (
-          <span
-            className="badge text-xs px-1"
-            style={{ backgroundColor: t.color }}
-          >
-            {t.text}
-          </span>
-        ))}
-      </ul>
-    </div>
   )
 }
