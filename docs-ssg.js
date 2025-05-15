1fd4765e49e7a9c17c7c2e75f02d30d11fbe3a6d
LankyMoose <rausten93@gmail.com>
2025-05-16 01:34:23 +1200

    re-impl ssg

--- docs-ssg.js
+++ docs-ssg.js
@@ -1,20 +1,57 @@
 import fs from "node:fs"
 import { render } from "./dist/server/entry-server.js"
 
+function pathToRoute(path) {
+  return path
+    .replace(/\.tsx$/, "")
+    .replace(/\.mdx$/, "")
+    .replace(/\/index$/, "/")
+    .replace(/\[(\w+)\]/g, ":$1")
+}
+
 const html = fs.readFileSync("./dist/client/index.html", "utf-8")
+const routes = fs
+  .readdirSync("./src/pages", {
+    recursive: true,
+  })
+  .map((r) => "/" + r.replace(/\\/g, "/"))
+  .map(pathToRoute)
 
-const index = html.replace(
-  "<body></body>",
-  `<body>${render({ url: "/" })}</body>`
-)
+console.log({ routes })
 
-//fs.writeFileSync("./dist/client/index.html", index)
+routes.forEach(async (route) => {
+  // we need to transform 'blog\\index.tsx' into '/blog/index.tsx'
+  if (route.split("/").length > 1) {
+    // ensure child dir is created
 
-// clone the contents of "./dist/client" into "./docs"
+    fs.mkdirSync("./dist/client/" + route.split("/").slice(0, -1).join("/"), {
+      recursive: true,
+    })
+  }
 
-fs.mkdirSync("./docs", { recursive: true })
-fs.cpSync("./dist/client", "./docs", { recursive: true })
+  const rendered = html.replace(
+    "<body></body>",
+    `<body>${await render({ path: route })}</body>`
+  )
 
-// replace "./docs/index.html" with the generated index.html
+  if (route.endsWith("/")) {
+    route += "index"
+  }
+  fs.writeFileSync("./dist/client" + route + ".html", rendered)
+})
 
-fs.writeFileSync("./docs/index.html", index)
+// const index = html.replace(
+//   "<body></body>",
+//   `<body>${render({ url: "/" })}</body>`
+// )
+
+// //fs.writeFileSync("./dist/client/index.html", index)
+
+// // clone the contents of "./dist/client" into "./docs"
+
+// fs.mkdirSync("./docs", { recursive: true })
+// fs.cpSync("./dist/client", "./docs", { recursive: true })
+
+// // replace "./docs/index.html" with the generated index.html
+
+// fs.writeFileSync("./docs/index.html", index)
