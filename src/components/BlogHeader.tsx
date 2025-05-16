f79282033105d663ad089589d1e47ec0ddadc7e1
LankyMoose <rausten93@gmail.com>
2025-05-16 14:31:56 +1200

    update blog header date display

--- src/components/BlogHeader.tsx
+++ src/components/BlogHeader.tsx
@@ -12,7 +12,9 @@ export function BlogHeader({ children, route }: BlogHeaderProps) {
       <div id="hero">
         <div className="section-content flex flex-col gap-2">
           <h1 className="not-prose">
-            <small className="text-sm text-spicy">{date}</small>
+            <small className="text-sm text-spicy">
+              {new Date(date).toDateString()}
+            </small>
             <big>{children}</big>
           </h1>
           <p className="text-muted" style="margin:0">
