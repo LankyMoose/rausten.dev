513f3c152cff5da13952798280b641312f552f29
LankyMoose <rausten93@gmail.com>
2025-06-10 09:03:12 +1200

    add <Head> for 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,9 +1,14 @@
 import { Link } from "$/components/Link"
 import { Hero } from "$/components/Hero"
+import { Head } from "$/components/Head"
 
 export default function Page() {
   return (
     <section className="flex flex-col p-4 min-h-[240px] sm:min-h-[320px] justify-end">
+      <Head>
+        <title>404 - rausten.dev</title>
+        <meta name="description" content="Page not found" />
+      </Head>
       <Hero
         sup="404"
         heading="Page not found"
