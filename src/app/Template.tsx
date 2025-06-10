edbd52c1c788a53f05aa34fdbc465ae51f683464
LankyMoose <rausten93@gmail.com>
2025-06-11 06:59:37 +1200

    improve footer

--- src/app/Template.tsx
+++ src/app/Template.tsx
@@ -38,7 +38,7 @@ export function Template({ children }: TemplateProps) {
       <main className="p-4 flex flex-col gap-6 grow w-full max-w-3xl lg:max-w-5xl mx-auto">
         {children}
       </main>
-      <footer className="flex flex-col gap-2 px-1">
+      <footer className="flex flex-col gap-2 px-1 py-2">
         <div className="flex gap-4 items-center mx-auto">
           <SocialLink href="https://www.github.com/lankymoose">
             <Github title="GitHub" />
@@ -53,7 +53,7 @@ export function Template({ children }: TemplateProps) {
             <Twitch title="Twitch" />
           </SocialLink>
         </div>
-        <div className="flex justify-end">
+        <div className="flex justify-center">
           <span className="font-light text-neutral-300">
             Made with{" "}
             <a href="https://kaioken.dev" target="_blank">
