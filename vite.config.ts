c6ef4026d13ec3db9371fc6898b43c5e0d5bd71c
LankyMoose <rausten93@gmail.com>
2025-06-12 03:26:59 +1200

    refactor `ssg.js` into vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -28,6 +28,7 @@ export default defineConfig({
       jsxRuntime: "automatic",
       remarkPlugins: [remarkFrontmatter],
     }),
+    SSG(),
   ],
 })
 
@@ -80,6 +81,89 @@ function blogDiscovery() {
     },
     async buildStart() {
       await scanArticles()
+    },
+  } satisfies Plugin
+}
+
+function SSG() {
+  return {
+    name: "vite:ssg",
+    apply: "build",
+    enforce: "post",
+
+    async closeBundle() {
+      if (!this.environment.config.build.ssr) return
+
+      console.log("[SSG] Starting static site generation...")
+
+      const distDir = path.resolve("./dist/server")
+      const rendererPath = path.join(distDir, "entry-server.js")
+      const { render } = await import(`file://${rendererPath}`)
+
+      const routes = (await fs.readdir("./src/pages", { recursive: true }))
+        .filter(
+          (filePath) => filePath.endsWith(".tsx") || filePath.endsWith(".mdx")
+        )
+        .map((filePath) => {
+          let file = filePath
+            .replace(/\\/g, "/")
+            .replace(/\.tsx$/, "")
+            .replace(/\.mdx$/, "")
+
+          if (file === "index") {
+            return "/"
+          }
+          if (file.endsWith("/index")) {
+            file = file.slice(0, -"/index".length)
+          }
+          return "/" + file
+        })
+        .sort()
+
+      pages: {
+        let html
+        try {
+          html = await fs.readFile("./dist/client/index.html", "utf-8")
+        } catch (error) {
+          throw new Error(
+            '[SSG] Failed to read "./dist/client/index.html". Ensure client build is complete.'
+          )
+        }
+        for (const route of routes) {
+          const pathParts = route.split("/").filter(Boolean)
+          if (pathParts.length > 1) {
+            const dir = pathParts.slice(0, -1).join("/")
+            await fs.mkdir("./dist/client/" + dir, { recursive: true })
+          }
+          const { body, head } = await render({ path: route })
+          const rendered = html
+            .replace("<!-- HEAD -->", head)
+            .replace("<body></body>", `<body>${body}</body>`)
+          let outputPath = route
+          if (route.endsWith("/")) {
+            outputPath += "index"
+          }
+          outputPath = `./dist/client${outputPath}.html`
+          await fs.writeFile(outputPath, rendered)
+          console.log("[SSG] created page", outputPath)
+        }
+      }
+      sitemap: {
+        const excludedSitemapRoutes = ["/404"]
+        const baseUrl = "https://rausten.dev"
+        const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+      <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      ${routes
+        .filter((route) => !excludedSitemapRoutes.includes(route))
+        .map((route) => {
+          const url = route === "/" ? baseUrl : `${baseUrl}${route}`
+          return `  <url><loc>${url}</loc></url>`
+        })
+        .join("\n")}
+      </urlset>\n`
+        await fs.writeFile("./dist/client/sitemap.xml", sitemap)
+        console.log("[SSG] Sitemap generated")
+      }
     },
   } satisfies Plugin
 }
