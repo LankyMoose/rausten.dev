f90d321c844ae3d3b9e129e78daaa8fb1f0d70f8
LankyMoose <rausten93@gmail.com>
2025-06-12 02:38:39 +1200

    refactor `ssg.js`, generate `sitemap.xml`

--- ssg.js
+++ ssg.js
@@ -1,31 +1,39 @@
 import fs from "node:fs"
 import { render } from "./dist/server/entry-server.js"
 
-function pathToRoute(path) {
-  return path
+function pathToRoute(filePath) {
+  let path = filePath
+    .replace(/\\/g, "/")
     .replace(/\.tsx$/, "")
     .replace(/\.mdx$/, "")
-    .replace(/\/index$/, "/")
-    .replace(/\[(\w+)\]/g, ":$1")
+
+  if (path === "index") {
+    return "/"
+  }
+
+  if (path.endsWith("/index")) {
+    path = path.slice(0, -"/index".length)
+  }
+
+  return "/" + path
 }
 
 const html = fs.readFileSync("./dist/client/index.html", "utf-8")
 const routes = fs
-  .readdirSync("./src/pages", {
-    recursive: true,
-  })
-  .map((r) => "/" + r.replace(/\\/g, "/"))
+  .readdirSync("./src/pages", { recursive: true })
   .map(pathToRoute)
+  .filter((item, idx, arr) => arr.indexOf(item) === idx)
+  .sort()
 
 console.log("SSG - routes", routes)
 
 routes.forEach(async (path) => {
-  if (path.split("/").length > 1) {
+  const pathParts = path.split("/").filter(Boolean)
+  if (pathParts.length > 1) {
     // ensure child dir is created
-
-    fs.mkdirSync("./dist/client/" + path.split("/").slice(0, -1).join("/"), {
-      recursive: true,
-    })
+    const dir = pathParts.slice(0, -1).join("/")
+    console.log("SSG - creating dir")
+    fs.mkdirSync("./dist/client/" + dir, { recursive: true })
   }
   const { body, head } = await render({ path })
 
@@ -38,3 +46,20 @@ routes.forEach(async (path) => {
   }
   fs.writeFileSync("./dist/client" + path + ".html", rendered)
 })
+
+const excludedSitemapRoutes = ["/404"]
+
+const baseUrl = "https://rausten.dev"
+const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${routes
+  .filter((route) => !excludedSitemapRoutes.includes(route))
+  .map((route) => {
+    const url = route === "/" ? baseUrl : `${baseUrl}${route}`
+    return `  <url><loc>${url}</loc></url>`
+  })
+  .join("\n")}
+</urlset>\n`
+
+fs.writeFileSync("./dist/client/sitemap.xml", sitemap)
+console.log("Sitemap generated:\n", sitemap)
