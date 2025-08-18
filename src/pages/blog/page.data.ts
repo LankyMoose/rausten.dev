a762e3673d7a8444dd02dc30087d61aae5d2018c
LankyMoose <rausten93@gmail.com>
2025-08-19 07:47:48 +1200

    fix blog link data sorting

--- src/pages/blog/page.data.ts
+++ src/pages/blog/page.data.ts
@@ -1,5 +1,9 @@
 import blogManifest from "virtual:blog-manifest"
 
 export const blogLinkData = Object.keys(blogManifest)
-  .sort((a, b) => new Date(b).getTime() - new Date(a).getTime())
+  .sort(
+    (a, b) =>
+      new Date(b.replace("/blog/", "")).getTime() -
+      new Date(a.replace("/blog/", "")).getTime()
+  )
   .map((path) => ({ path, ...blogManifest[path] }))
